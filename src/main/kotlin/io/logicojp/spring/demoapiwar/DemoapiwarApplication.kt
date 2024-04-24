3168605074cb809737e4eda6bf04cf37cf592686
anishi1222 <a_nishi-anarch@bc4.so-net.ne.jp>
2024-04-24 15:04:57 +0900

    Initial commit

---
+++ src/main/kotlin/io/logicojp/spring/demoapiwar/DemoapiwarApplication.kt
@@ -0,0 +1,11 @@
+package io.logicojp.spring.demoapiwar
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoapiwarApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoapiwarApplication>(*args)
+}
---
+++ src/test/kotlin/io/logicojp/spring/demoapiwar/DemoapiwarApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.logicojp.spring.demoapiwar
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoapiwarApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
